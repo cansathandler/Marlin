9a951d76741a97e3355cc8919116db42b2d35557
Ryo Kanno <tdu.aqa1@gmail.com>
2024-09-17 10:20:27 +0200

    no message

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2022,8 +2022,8 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
-  #define GRID_MAX_POINTS_Y 6
+  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_Y 7
 
   // Probe along the Y axis, advancing X after each column
   //#define PROBE_Y_FIRST
